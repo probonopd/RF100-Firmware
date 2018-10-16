1ce0a5a610ae76cafc7bebb0018602bf4ae9447d
probonopd <probonopd@users.noreply.github.com>
2018-10-16 19:50:15 +0200

    EXTRUDER_AUTO_FAN_SPEED   100

--- RF-100/example_configurations/Renkforce/RF100/Configuration_adv.h
+++ RF-100/example_configurations/Renkforce/RF100/Configuration_adv.h
@@ -218,7 +218,7 @@
 #define E3_AUTO_FAN_PIN -1
 #define E4_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 80
-#define EXTRUDER_AUTO_FAN_SPEED   64  // 255 == full speed
+#define EXTRUDER_AUTO_FAN_SPEED   100  // 255 == full speed
 
 /**
  * Part-Cooling Fan Multiplexer
